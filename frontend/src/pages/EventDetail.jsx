bac9af648e3bb8a72779dc125545c45249944147
Gurdyal006 <gurdyal.singh@tmotions.com>
2024-02-12 12:50:54 +0530

    add-links-populate-with-id

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,17 +1,29 @@
-import { Link, useParams } from "react-router-dom";
+import { useRouteLoaderData, json } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useRouteLoaderData("event-detail");
+  console.log(data, "data---");
 
-  return (
-    <>
-      <h1>Event details</h1>
-      <p>ID: {params.eventId}</p>
-      <Link to=".." relative="path">
-        back
-      </Link>
-    </>
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
